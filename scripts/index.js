2f019189223ec37f2983951e7ac470ff2ff2c002
press 912 sbr <d21r34@att.net>
2024-06-20 22:52:43 -0400

    sprint 4 project stage 3 finished

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,18 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const profileEditCloseButton = document.querySelector(
+  "#profile-edit-close-button"
+);
+
+profileEditCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
