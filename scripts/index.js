498f8beec97523f107b628f372d0bbf9a4d8ba96
press 912 sbr <d21r34@att.net>
2024-06-25 21:04:34 -0400

    added modal functions

--- scripts/index.js
+++ scripts/index.js
@@ -27,15 +27,36 @@ const initialCards = [
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal__opened");
-});
-
 const profileEditCloseButton = document.querySelector(
   "#profile-edit-close-button"
 );
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
-profileEditCloseButton.addEventListener("click", () => {
+function closePopup() {
   profileEditModal.classList.remove("modal__opened");
+}
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  profileEditModal.classList.add("modal__opened");
 });
+
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
