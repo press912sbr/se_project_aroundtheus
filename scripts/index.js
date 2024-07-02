3c7062d1f66c352a25518fabc1020adbcf3e7e9b
press 912 sbr <d21r34@att.net>
2024-07-01 22:50:19 -0400

    sprint 4 project finished 3

--- scripts/index.js
+++ scripts/index.js
@@ -42,7 +42,7 @@ const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
 function closePopup() {
-  profileEditModal.classList.remove("modal_modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function handleProfileEditSubmit(e) {
@@ -67,7 +67,7 @@ function getCardElement(cardData) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal_modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileEditCloseButton.addEventListener("click", closePopup);
